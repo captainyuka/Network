bb405711507c21f04127a70dc43a5a192e0828e4
captainyuka <70305009+captainyuka@users.noreply.github.com>
2020-08-31 14:34:26 +0300

    Create DataSample.java

---
+++ 03_Adaptive_Stream_Compression/DataSample.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DataSample{
+  long byteCount;
+  Date start;
+  Date end;
+
+  DataSample(long byteCount, Date start,Date end){
+    this.byteCount = byteCount;
+    this.start = start;
+    this.end = end;
+  }
+}
